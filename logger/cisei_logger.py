d3082953bee77e85da65603348e50da54df0910d
Jan Borowiec <janekborowiec@gmail.com>
2021-09-01 16:30:04 +0200

    Added loger for person info

---
+++ logger/cisei_logger.py
@@ -0,0 +1,79 @@
+import psycopg2
+from scrapers.cisei.cisei_scrapper import PersonalInfo
+
+
+class LoggerDB:
+    HOST = "database_pg"
+    PORT = "5432"
+    DB_NAME = "database_pg"
+    USER = "postgres"
+    PASSWORD = "postgres"
+
+    def __init__(self, host=HOST, db_name=DB_NAME, port=PORT, user=USER, password=PASSWORD) -> None:
+        self.conn = psycopg2.connect(host=host, database=db_name, port=port, user=user, password=password)
+        self.c = self.conn.cursor()
+
+        self.create_person_info_table()
+
+    def create_person_info_table(self):
+        self.c.execute(
+            """
+            CREATE TABLE IF NOT EXISTS person_info (
+            id SERIAL PRIMARY KEY,
+            idx TEXT NOT NULL,
+            surname TEXT NOT NULL,
+            full_name TEXT NOT NULL,
+            age INT NOT NULL,
+            trip_date DATE,
+            registration_place TEXT,
+            url TEXT
+            );
+            """
+        )
+
+    def add_person_info(self, person_info: PersonalInfo) -> None:
+        with self.conn:
+            self.c.execute(
+                """
+            INSERT INTO person_info DATA(
+                id,
+                idx,
+                surname,
+                full_name,
+                age,
+                trip_date,
+                registration_place,
+                url
+            )
+            VALUES (DEFAULT, %s, %s, %s, %s, %s, %s, %s);
+            """,
+                (
+                    person_info.idx,
+                    person_info.surname,
+                    person_info.full_name,
+                    person_info.age,
+                    person_info.trip_date,
+                    person_info.registration_place,
+                    person_info.url,
+                ),
+            )
+
+    def display_last_hours_taker_opportunity(self, hours: int):
+        with self.conn:
+            self.c.execute(
+                """
+                SELECT
+                    id,
+                    idx,
+                    surname,
+                    full_name,
+                    age,
+                    trip_date,
+                    registration_place,
+                    url
+                FROM person_info
+                ORDER BY registration_place;
+                """
+            )
+
+            return self.c.fetchall()
